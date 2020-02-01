07c7cfd8180cb0d1038cea26fdc11f8235723578
BCR <frc294@gmail.com>
2020-02-01 15:47:43 -0800

    Added camera and hopper motor reverse

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -121,10 +121,12 @@ public class RobotContainer {
     }
 
     // A = 1, B = 2, X = 3, Y = 4
-    xb[1].whenPressed(new FeederSetPiston(false, feeder));
+    ///xb[1].whenPressed(new FeederSetPiston(false, feeder));
+    xb[1].whenHeld(new HopperSetPercentOutput(-0.8, hopper));
+    xb[1].whenReleased(new HopperSetPercentOutput(hopper));
     xb[2].whenPressed(new ShooterFeederHopperSequenceNoPiston(shooter, feeder, hopper, intake));
     xb[3].whenPressed(new ShooterFeederHopperStop(shooter, feeder, hopper));
-    xb[4].whenPressed(new FeederSetPiston(true, feeder));
+    //xb[4].whenPressed(new FeederSetPiston(true, feeder));
 
     // LB = 5, RB = 6
     // xb[5].whenPressed(new Wait(0));
