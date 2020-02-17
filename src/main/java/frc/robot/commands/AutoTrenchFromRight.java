78368e6a639ae7d7b6a6f6b0c567f72cd0d578b6
Don Sawdai <dsawdai@ieee.org>
2020-02-16 22:09:03 -0800

    Fixed compile bug

--- src/main/java/frc/robot/commands/AutoTrenchFromRight.java
+++ src/main/java/frc/robot/commands/AutoTrenchFromRight.java
@@ -15,7 +15,7 @@ public class AutoTrenchFromRight extends SequentialCommandGroup {
    * AutoTrench constructor for command group
    * @param driveTrain  The driveTrain to use to get the pose, wheel speeds and set the tankDriveVolts
    */  
-  public AutoTrenchFromRight(DriveTrain driveTrain, FileLog log, Trajectory trajectory, DifferentialDriveKinematics driveKinematics) {
+  public AutoTrenchFromRight(DriveTrain driveTrain, FileLog log, Trajectory trajectory) {
 
     addCommands(
       new DriveFollowTrajectory(trajectory, true, true, driveTrain, log)
