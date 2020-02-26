80b6b56c07f8378b700a4c22a636a3b0a7e6b946
Audrey Lee <2021leea@gmail.com>
2020-02-25 18:47:26 -0800

    motorCheck stuff

--- src/main/java/frc/robot/utilities/MotorCheck.java
+++ src/main/java/frc/robot/utilities/MotorCheck.java
@@ -10,27 +10,96 @@ package frc.robot.utilities;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+
 /**
  * Add your docs here.
  */
 public class MotorCheck {
+    private static String flaggedMotors;
+
+    private FileLog log;
+    private PowerDistributionPanel pdp;
+
     private WPI_TalonFX motor1;
     private WPI_TalonFX motor2;
     private WPI_VictorSPX victorMotor;
     private int victorMotorPDP;
-    private int motor2PDP;
-    private String subsystemChecked;
 
-    public MotorCheck(WPI_TalonFX motor1, WPI_TalonFX motor, String subsystem) {
-        this.motor1 = motor1;
-        this.motor2 = motor2;
-        this.subsystemChecked = subsystem;
+    private int currentFaultCount;
+
+    private String subsystemChecked;
+    private boolean oneMotor; // true is one Motor, false is two Motors
+
+    public MotorCheck(WPI_TalonFX motor1, WPI_TalonFX motor2, String subsystem, FileLog log) {
+      this.motor1 = motor1;
+      this.motor2 = motor2;
+      this.subsystemChecked = subsystem;
+      this.log = log;
     }
 
-    public MotorCheck(WPI_VictorSPX victorMotor, int victorMotorPDP, String subsystem) {
-        this.victorMotor = victorMotor;
-        this. victorMotorPDP = victorMotorPDP;
-        this.subsystemChecked = subsystem;
+    public MotorCheck(WPI_TalonFX motor1, String subsystem, FileLog log) {
+      this.motor1 = motor1;
+      this.motor2 = null;
+      this.subsystemChecked = subsystem;
+      this.log = log;
+    }
+
+    public MotorCheck(WPI_VictorSPX victorMotor, int victorMotorPDP, String subsystem, FileLog log, PowerDistributionPanel pdp) {
+      this.victorMotor = victorMotor;
+      this. victorMotorPDP = victorMotorPDP;
+      this.subsystemChecked = subsystem;
+      this.log = log;
+      this.pdp = pdp;
+    }
+
+    public void checkMotorCurrents(double diffThresh, double minThresh, double maxThresh) {
+      double amps1 = motor1.getStatorCurrent();
+      double amps2 = motor2.getStatorCurrent();
+      double difference = Math.abs(amps1 - amps2);
+
+      if (difference > diffThresh) {
+        currentFaultCount++;
+      } else {
+        currentFaultCount = 0;
+      }
+
+      if(currentFaultCount >= 10) {
+        findCulprit(minThresh, maxThresh, amps1, amps2);
+        RobotPreferences.recordStickyFaults(subsystemChecked, log);
+      }
+    }
+
+    private void findCulprit(double minThresh, double maxThresh, double amps1, double amps2) {
+      if (amps1 < minThresh) {
+        logMotorFault(subsystemChecked, "Motor1LowCurr");
+      } else if (amps2 < minThresh) {
+        logMotorFault(subsystemChecked, "Motor2LowCurr");
+      } else if (amps1 > maxThresh) {
+        logMotorFault(subsystemChecked, "Motor1HighCurr");
+      } else if (amps2 > maxThresh) {
+        logMotorFault(subsystemChecked, "Motor2HighCurr");
+      } else {
+
+      }
+    }
+
+    public void checkSingleMotorCurrent(double minThresh, double maxThresh, boolean victorMotor) {
+      double amps;
+      double direction;
+      if(victorMotor) {
+        
+      }
+    }
+
+    public void logMotorFault(String subsystem, String motorIssue) {
+      if (flaggedMotors.indexOf(motorIssue) == -1) {
+        if (flaggedMotors.length() != 0) {
+          flaggedMotors = flaggedMotors + ", ";
+        }
+        flaggedMotors = flaggedMotors + motorIssue;
+        log.writeLog(true, subsystem, "Sticky Faults Logged", motorIssue);
+      }
     }
 
     public void checkMotors() {
