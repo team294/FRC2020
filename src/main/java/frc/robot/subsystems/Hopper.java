a75f11ce11da1a56c4f0f2a51008d73d595e251d
BCR <frc294@gmail.com>
2020-02-28 22:05:01 -0800

    Revert "Merge remote-tracking branch 'origin/Jenna-PistonDelay' into Audrey-TimerFixes"
    This reverts commit 8f4a09fcd193459c8f305af1041df7cadc05ece4, reversing
    changes made to 4530f32c5817fafd187c4f8ef199594e5d6ea1ed.

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -9,7 +9,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.HopperConstants.*;
 
 public class Hopper extends SubsystemBase {
-  private final WPI_TalonSRX hopperMotor = new WPI_TalonSRX(canHopperMotor);
+  private final WPI_VictorSPX hopperMotor = new WPI_VictorSPX(canHopperMotor);
   
   public Hopper() {
     hopperMotor.configFactoryDefault();
